1e61128ed40b9f3479e558ecf324dd0a9d771d80
rezagumilar158 <rezagumilar158@gmail.com>
2023-04-20 06:46:07 +0700

    update patch

--- service-media/models/index.js
+++ service-media/models/index.js
@@ -19,12 +19,7 @@ if (config.use_env_variable) {
 fs
   .readdirSync(__dirname)
   .filter(file => {
-    return (
-      file.indexOf('.') !== 0 &&
-      file !== basename &&
-      file.slice(-3) === '.js' &&
-      file.indexOf('.test.js') === -1
-    );
+    return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
   })
   .forEach(file => {
     const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
--- service-media/routes/media.js
+++ service-media/routes/media.js
@@ -2,22 +2,39 @@ const express = require('express');
 const router = express.Router();
 const isBase64 = require('is-base64');
 const base64Img = require('base64-img');
+const fs = require('fs');
 
 const {Media} = require('../models');
 
+router.get('/', async(req, res) =>{
+  const media = await Media.findAll({
+    attributes: ['id', 'image']
+  });
 
-/* GET users listing. */
-router.post('/',(req, res) => {
+  const mappedMedia = media.map((m) => {
+    m.image = `${req.get('host')}/${m.image}`;
+    return m;
+  });
+
+  return res.json({
+    status: 'success',
+    data: mappedMedia
+  });
+}); 
+
+
+
+router.post('/', (req, res) => {
   const image = req.body.image;
 
   if (!isBase64(image, { mimeRequired: true })) {
     return res.status(400).json({ status: 'error', message: 'invalid base64' });
   }
+
   base64Img.img(image, './public/images', Date.now(),  async (err, filepath) => {
     if (err) {
       return res.status(400).json({ status: 'error', message: err.message });
     }
-
 
     const filename = filepath.split("\\").pop().split('/').pop();
 
@@ -32,6 +49,32 @@ router.post('/',(req, res) => {
     });
 
   })
+});
+
+
+router.delete('/:id', async (req, res)=> {
+  const id = req.params.id; 
+
+  const media = await Media.findByPk(id);
+
+  if (!media) {
+    return res.status(400).json({status: 'error', message: 'media not found'});
+  }
+
+  fs.unlink(`./public/${media.image}`, async (err) => {
+    if (err) {
+      return res.status(400).json({status: 'error', message: err.message });
+    }
+
+    await media.destroy();
+
+    return res.json({
+      status: 'success',
+      message: 'image deleted'
+    });
+
+  });
+
 });
 
 module.exports = router;
